02810fb24408394d6b0a913ac7756936f2b6b3e1
Maurício Spagnol <mauriciogspagnol@gmail.com>
2021-03-10 21:38:17 -0300

    alt

---
+++ server.js
@@ -0,0 +1,24 @@
+const env = require("./src/env");
+const express = require("express");
+const app = express();
+const rotas = require("./src/rotas/importacaoRotas");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const autenticacao = require("./src/middlewares/autenticacao");
+const connectionMiddleware = require("./src/middlewares/connection-middleware");
+
+//libera acesso de qualquer lugar
+app.use(cors());
+
+//autenticação por token
+app.use(autenticacao);
+app.use(connectionMiddleware);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+//rotas
+app.use(rotas);
+
+app.listen(process.env.PORT || 3001);
