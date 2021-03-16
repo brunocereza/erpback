2382f88e7be61c4431036586e5109286dd8447a3
Mauricio503 <mauriciogspagnol@gmail.com>
2021-03-16 13:06:18 -0300

    alterando url conexão

--- src/middlewares/connection-middleware.js
+++ src/middlewares/connection-middleware.js
@@ -6,7 +6,7 @@ module.exports = async(req, res, next) => {
   const prisma = new PrismaClient({
     datasources: {
       db: {
-        url: "postgresql://postgres:postgres@localhost:5432/newT?schema=public",
+        url: process.env.DATABASE_URL+req.schema,
       },
     },
   });      
