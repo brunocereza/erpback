d95e36ca55b5e793fc1fc854d1867446687ef5cf
Maurício Spagnol <mauriciogspagnol@gmail.com>
2021-03-31 13:52:28 -0300

    task6 em andamento

--- src/controller/cadastro/PessoaController.js
+++ src/controller/cadastro/PessoaController.js
@@ -17,4 +17,27 @@ module.exports = {
         await req.connection.$disconnect()
     })
   },
+  async salvar(req,res){
+    async function main(req) {
+      const {logradouro,cep,bairro,numero,complemento,cidade_id,estado_id,
+        cpf_cnpj,nome,nome_fantasia,telefone,email} = req.body;
+      const pessoa = await req.connection.pessoa.create({data:{cpf_cnpj,nome,nome_fantasia,
+        endereco: {
+          create:{logradouro,cep,bairro,numero,
+            complemento,cidade_id,estado_id},
+        },
+        email,telefone}});
+      return pessoa
+    }
+    main(req)
+    .then(resp => {
+        res.send(resp);
+    })
+    .catch(e => {
+        throw e
+    })
+    .finally(async () => {
+        await req.connection.$disconnect()
+    })
+  }
 }
--- src/rotas/cadastro/PessoaRoute.js
+++ src/rotas/cadastro/PessoaRoute.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 const PessoaController = require("../../controller/cadastro/PessoaController");
 
 routes.post("/pessoa/lista", PessoaController.lista);
+routes.post("/pessoa/salvar", PessoaController.salvar);
 
 module.exports = routes;
